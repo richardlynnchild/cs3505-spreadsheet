4fc7497a03f9aa8d838372687f68fcc80d8d4234
megane <megane@lab1-29.eng.utah.edu>
2018-04-04 15:21:58 -0600

    Finished basic structure for spreadsheet.cpp and spreadsheet.h

--- Server/spreadsheet.h
+++ Server/spreadsheet.h
@@ -4,19 +4,23 @@
 #include <sys/socket.h>
 #include <pthread.h>
 #include <string>
+#include <map>
+#include <vector>
+#include <stack>
+
 class Spreadsheet
 {
  private:
-  std::map<std::string, std::string> spreadsheet_state;
+  std::map < std::string, std::string > spreadsheet_state;
   std::vector<std::string> clients;
-  std::map<std::string, std::stack<std::string>> revert_stacks;
-  std::stack<std::pair<std::string,std::string>> undo_stack;
+  std::map<std::string, std::stack<std::string> > revert_stacks;
+  std::stack<std::pair <std::string,std::string> > undo_stack;
  public:
   Spreadsheet(std::string name);
   void EditSheet(std::string cell_name, std::string contents);
-  std::string Undo();
+  std::pair<std::string, std::string> Undo();
   std::string Revert(std::string cell_name);
 
 };
 
-#endif SPREADSHEET_H
+#endif
