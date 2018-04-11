ed3a1f1a1771bdbd2edd97b1c2e3417ef7ee6be8
ballred <nihilian@live.com>
2018-04-11 12:11:11 -0600

    Separated networking code

--- Server/listen_test.cpp
+++ Server/listen_test.cpp
@@ -5,10 +5,9 @@
 int main(int argc, char* argv[])
 {
   std::string input;
-  int port = 2112;
   
   std::cout << "Starting test" << std::endl;
-  Lobby lobby(2112);
+  Lobby lobby;
   std::cout << "Created Lobby" << std::endl;
   lobby.Start();
   std::cout << "Server started" << std::endl;
