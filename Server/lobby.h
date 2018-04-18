ab6c31d1adf507d07ccbb9d1f112f120517ca87c
richardlynnchild <richardlynnchild@gmail.com>
2018-04-18 10:54:02 -0600

    Fixed some compiler issues in lobby.cpp and lobby.h

--- Server/lobby.h
+++ Server/lobby.h
@@ -31,7 +31,7 @@ class Lobby {
     std::string BuildUnfocus();
     bool CheckForMessages();
     void HandleMessage(std::string message,std::string sheet);
-    std::vector<std:string> SplitString(std::string str, char delim);
+    void SendChangeMessage(std::string message, std::string sheet);
 
   public:
     Lobby();
