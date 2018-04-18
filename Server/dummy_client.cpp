3cb0be4705e302c53f2697a20e26a9628e221f52
ballred <nihilian@live.com>
2018-04-18 08:54:02 -0600

    Fixed makefile

---
+++ Server/dummy_client.cpp
@@ -0,0 +1,52 @@
+#include <iostream>
+#include <string>
+#include <cstring>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <errno.h>
+#include <stdlib.h>
+
+int main(int argc, char* argv[])
+{
+  
+  int sock_descriptor = socket(AF_INET, SOCK_STREAM, 0);
+  struct sockaddr_in address;
+  address.sin_family = AF_INET;
+  address.sin_addr.s_addr = INADDR_ANY;
+  address.sin_port = htons(2112);
+  socklen_t address_len = sizeof(address);
+
+  if (connect(sock_descriptor, (struct sockaddr*) &address, address_len) != 0)
+  {
+    std::cout << "Cannot connect!" << std::endl;
+    return 0;
+  }
+  else
+  {
+    std::cout << "Connected" << std::endl;
+  }
+
+  char ext = (char) 3;
+  char buffer[9] = "register";
+  buffer[8] = ext;
+
+ if ( send(sock_descriptor, buffer, 9, 0) < 0)
+    std::cout << strerror(errno) << std::endl;
+ else
+  {
+    std::cout << "Sent register" << std::endl;
+  }
+
+  char buffer2[2048];
+  int bytes = recv(sock_descriptor, &(buffer2[0]), 2048, 0);
+  std::cout << buffer2 << std::endl;
+
+  char buffer3[11] = "load test1";
+  buffer3[10] = ext;
+ if ( send(sock_descriptor, buffer3, 11, 0) < 0)
+    std::cout << strerror(errno) << std::endl;
+ else
+  {
+    std::cout << "Sent load" << std::endl;
+  }
+}
